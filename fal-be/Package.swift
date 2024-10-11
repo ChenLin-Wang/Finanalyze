15eb7c1b5f02c7e6173ba71ec8c443e0d2b1ffc6
chenlin-wang <wangchenlin2001@icloud.com>
2024-10-11 02:33:12 +0000

    init ai support

--- fal-be/Package.swift
+++ fal-be/Package.swift
@@ -13,6 +13,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
+        .package(url: "https://github.com/google/generative-ai-swift", from: "0.5.6"),
+
     ],
     targets: [
         .executableTarget(
@@ -23,6 +25,7 @@ let package = Package(
                 .product(name: "NIOPosix", package: "swift-nio"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "GoogleGenerativeAI", package: "generative-ai-swift"),
             ],
             swiftSettings: swiftSettings
         ),
---
+++ fal-be/Sources/Ai/Chat.swift
@@ -0,0 +1,8 @@
+import Vapor
+import GoogleGenerativeAI
+
+let model = GenerativeModel(name: "gemini-1.5-flash", apiKey: "AIzaSyB7j2gX8SNy5AxNHdiXp9x9gS5mNj3Fcqk")
+
+// func chat(prompts: [], history: [ModelContent]) {
+//     ModelContent
+// }
